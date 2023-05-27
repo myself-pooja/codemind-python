d6bcb13e0bd02a63c97fa9eb4112a474196a11ea
myself-pooja <128219224+myself-pooja@users.noreply.github.com>
2023-05-27 20:21:06 +0530

    l=input().lower().split() s=''.join(l) a='abcdefghijklmnopqrstuvwxyz' d=[] for i in a:     if i in s:         d.append(i) for i in d:     print(i,end="")

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,10 +1,9 @@
 l=input().lower().split()
 s=''.join(l)
-c=[]
-for i in s:
-    if s.count(i)==1:
-        c.append(i)
-c.sort()
-for i in c:
+a='abcdefghijklmnopqrstuvwxyz'
+d=[]
+for i in a:
+    if i in s:
+        d.append(i)
+for i in d:
     print(i,end="")
-        
